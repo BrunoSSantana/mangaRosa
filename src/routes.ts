0ba71ddb10f99d6d62f3071245b44747d9a729ab
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-27 15:53:20 -0300

    Preparing routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,15 @@ import { Router } from "express";
 import {
   createUserController,
   findAllUsersController,
+  updateUser,
 } from "./controllers/UserController";
 
 const router = Router();
-
+// Users
 router.post("/registrar", createUserController);
+// Admin
+router.get("/registros", findAllUsersController);
+router.get("/:name/validar"); // PENDENTE
 router.put("/:name/validar", updateUser);
 
 export { router };
