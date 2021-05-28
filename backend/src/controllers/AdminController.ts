b3bb8f59bf37cc6fa2fb46eaff34db808cf30f08
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-28 09:12:36 -0300

    Separates controllers

--- backend/src/controllers/AdminController.ts
+++ backend/src/controllers/AdminController.ts
@@ -1,4 +1,60 @@
+import { Response, Request } from "express";
+
+import {
+  findAllusers,
+  findByName,
+  validateUser,
+} from "../services/UsersServices";
+
 const findAllUsersController = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  try {
+    const allUsers = await findAllusers();
+    return response.status(200).json(allUsers.rows);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+};
+
 const findUserController = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  try {
+    const { name } = request.params;
+    const user = await findByName(name);
+
+    return response.status(200).json(user);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+};
+
 const updateUserController = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  try {
+    const { name } = request.params;
+
+    // capturando todos os usuários
+    const allUsers = await findAllusers();
+
+    // pegando o usuário que possui o name igual ao que está na rota
+    // para isso uso regex para tirar os espaaços em branco dos nomes no banco de dados
+    const myUser = await allUsers.rows.find((obj) => {
+      return obj.name.replace(/\s/g, "") === name;
+    });
+
+    // validadando usuário
+    validateUser(myUser.name);
+
+    return response.json("User Updated Successfully");
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+};
+
 export { findAllUsersController, findUserController, updateUserController };
--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,3 +1,32 @@
+import { Response, Request } from "express";
+
+import { CpfValidation, emailValidation } from "../lib";
+import { createUser, getByCpf } from "../services/UsersServices";
+
+const createUserController = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  try {
+    const { name, email, cpf, cell, skills } = request.body;
+
+    // verifica se o usuário existe
+    const useralreadyExists = await getByCpf(cpf);
+
+    // Verifica se  usuário existe
+    if (useralreadyExists.rows[0] !== undefined) {
+      return response.status(400).json({ message: "Usuário exite" });
+    }
+
+    // valida o email
+    if (!emailValidation(email))
+      return response.status(400).json({ message: "Email inválido" });
+
+    // valida cpf
+    if (!CpfValidation(cpf)) {
+      return response.status(400).json({ message: "CPF inválido" });
+    }
+
     // valida se existe skills
     if (!skills || skills === [""]) {
       return response.status(400).json({ message: "skills Vazias" });
@@ -11,3 +40,14 @@
 
     // validate = false (default)
     const validate = false;
+
+    await createUser(name, email, cpf, cell, skills, validate);
+
+    // enviado apenas a linha do retorno do getBycpf
+    return response.status(201).json({ message: "criado com sucesso" });
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+};
+
+export { createUserController };
